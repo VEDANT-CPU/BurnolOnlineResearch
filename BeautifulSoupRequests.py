a750e17dad8a89825439c5ff38fe9e78da17dfb5
VEDANT-CPU <vedantpawar896@gmail.com>
2026-02-11 23:06:25 +0530

    Mimicing Browser

--- BeautifulSoupRequests.py
+++ BeautifulSoupRequests.py
@@ -1,3 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 
+more_pen_url = 'https://www.morepen.com/aboutus'
+#requests.get(more_pen_url)
+
+#websites block automated python requests. we need to make it mimic Chrome!!
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/120.0.0.0 Safari/537.36"
+}
+
+response = requests.get(more_pen_url, headers=headers)
+print(response.status_code)
+print(response.text[:500])#to print first 500 chars of the response object received from requests.get
+#stored in the response variable.
